93b4b02618d42c47ec0a721439aa52dc4510a7c2
GewinCodeX <chibuzornworie85@gmail.com>
2024-01-13 16:50:23 +0100

    chore: add delete user enhancement

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,26 +9,30 @@ const Home = () => {
   const [filteredData, setFilteredData] = useState([]);
   const [postTitle, setPostTitle] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
+  const [successMessage, setSuccessMessage] = useState("");
 
   const handleToggle = () => setToggle(!toggle);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://dummyapi.io/data/v1/user?limit", {
-          headers: {
-            "app-id": "65a19335e135fe610e0131e7",
-          },
-        });
-        const data = await response.json();
-        setUserData(data.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+  // GET_USER
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://dummyapi.io/data/v1/user?limit", {
+        headers: {
+          "app-id": "65a19335e135fe610e0131e7",
+        },
+      });
+      const data = await response.json();
+      setUserData(data.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
+  useEffect(() => {
     fetchData();
   }, []);
+
+  // filteredData
 
   useEffect(() => {
     const filteredResults = userData.filter((user) =>
@@ -44,10 +48,48 @@ const Home = () => {
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
+
+  // DELETE_USER
+  const deleteUser = async (userId) => {
+    try {
+      const response = await fetch(
+        `https://dummyapi.io/data/v1/user/${userId}`,
+        {
+          method: "DELETE",
+          headers: {
+            "app-id": "65a19335e135fe610e0131e7",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.ok) {
+        setSuccessMessage("User deleted successfully");
+        fetchData();
+      } else {
+        console.error("Error deleting user:", response.statusText);
+        setSuccessMessage("Error deleting user");
+      }
+    } catch (error) {
+      console.error("Error deleting user:", error.message);
+      setSuccessMessage("Error deleting user");
+    }
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSuccessMessage("");
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [successMessage]);
+
   return (
     <>
       <div className=" bg-[url('/src/components/assets/skyblue.jpeg')] bg-center bg-cover bg-no-repeat h-[100vh] w-[100%]">
-        <p className=" flex text-center justify-center text-[rgb(10,93,113)] pt-5">STACKBUILD BLOGG APP</p>
+        <p className=" flex text-center justify-center text-[rgb(10,93,113)] pt-5">
+          STACKBUILD BLOGG APP
+        </p>
         <div className=" flex justify-center items-center gap-10 pt-[5rem]">
           <div className=" flex justify-center">
             <input
@@ -107,6 +149,10 @@ const Home = () => {
           </form>
         </div>
 
+        {successMessage && (
+          <div className="text-[red] text-center mt-4">{successMessage}</div>
+        )}
+
         {/* Display filtered user data */}
         <div className="text-white flex flex-wrap mt-24">
           {currentItems.map((user) => (
@@ -123,7 +169,12 @@ const Home = () => {
                 Name: {user.title} {user.firstName} {user.lastName}
               </p>
               <div className=" flex gap-5">
-                <button className=" bg-[red] p-1 rounded-md">
+                <button
+                  onClick={() => {
+                    deleteUser(user.id);
+                  }}
+                  className=" bg-[red] p-1 rounded-md"
+                >
                   delete post
                 </button>
                 <button className=" bg-[rgb(10,93,113)] p-1 rounded-md">
